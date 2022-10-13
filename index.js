bbb6d5deeb83b954cc3f87d14e8b7c6db1ab773d
dodol26 <tonni.lius26@gmail.com>
2022-10-13 16:47:56 +0700

    revisi for loop ke map dan mengganti beberapa nama variabel

--- index.js
+++ index.js
@@ -22,53 +22,62 @@ let inputPassword = document.getElementById(password-input)
 let inputEmail = document.getElementById(email-input)
 
 function signUp(inputUser, inputPassword, inputEmail){
-    dbUser[inputUser] = {
-        password: inputPassword,
-        email: inputEmail,
-        type: "member",
-    }
-}
-
-let namaItem = document.getElementById(nama-item-input)
-let harga = document.getElementById(harga-input)
-let img = document.getElementById(img-input)
-
-function addItem(namaItem, harga, img){
-    dbItem.push({namaItem: namaItem, harga: harga, img: img, promo: false})
-}
-
-function addPromo(namaItem){
-    for(let i = 0; i < dbItem.length; i++){
-        if (namaItem === dbItem[i].namaItem){
-            dbItem[i].promo = true
-            dbItem[i]["hargaPromo"] = (50/100)*dbItem[i].harga
+    if (dbUser[inputUser]){
+        alert("User sudah ada, mohon gunakan user lain")
+    }else {
+        dbUser[inputUser] = {
+            password: inputPassword,
+            email: inputEmail,
+            type: "member",
         }
     }
 }
 
-function removePromo(namaItem){
-    for(let i = 0; i < dbItem.length; i++){
-        if (namaItem === dbItem[i].namaItem){
-            dbItem[i].promo = false
-            delete dbItem[i].hargaPromo
+let namaItemInput = document.getElementById(nama-item-input)
+let hargaInput = document.getElementById(harga-input)
+let imgInput = document.getElementById(img-input)
+
+function addItem(namaItemInput, hargaInput, imgInput){
+    dbItem.push({namaItem: namaItemInput, harga: hargaInput, img: imgInput, promo: false})
+}
+
+function addPromo(namaItemInput){
+    dbItem.map((item)=>{
+        const {name, harga, image, promo} = item
+        if (namaItemInput === name){
+            promo = true
+            item["hargaPromo"] = (50/100)*harga
         }
-    }
+
+    })
 }
 
-function editItem(namaItem, harga, img){
-    for(let i = 0; i < dbItem.length; i++){
-        if (namaItem === dbItem[i].namaItem){
-            dbItem[i].namaItem = namaItem
-            dbItem[i].harga = harga
-            dbItem[i].img = img
-        }        
-    }
+function removePromo(namaItemInput){
+    dbItem.map((item)=>{
+        const {name, harga, image, promo, hargaPromo} = item
+        if (namaItemInput === name){
+            promo = false
+            delete hargaPromo
+        }
+    })
 }
 
-function deleteItem(namaItem){
-    for(let i = 0; i < dbItem.length; i++){
-        if (namaItem === dbItem[i].namaItem){
-            delete dbItem[i]
+function editItem(namaItemInput, hargaInput, imgInput){
+    dbItem.map((item)=>{
+        const {name, harga, image, promo} = item
+        if (namaItemInput === name){
+            name = namaItemInput
+            harga = hargaInput
+            image = imgInput
         }        
-    }
+    })
+}
+
+function deleteItem(namaItemInput){
+    dbItem.map((item)=>{
+        const {name, harga, image, promo} = item
+        if (namaItemInput === name){
+            delete item
+        }        
+    })
 }
