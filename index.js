7be0931ad7912714728914dbb4e61fcea09fa89f
kibuelll <kibuellyoi34@gmail.com>
2022-10-13 21:18:42 +0700

    fix-bug

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 let dbItem = [
-    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false}
+    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false},
+    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false},
+    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false},
+    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false},
 ]
+
 
 let dbUser = {
     dodol: {password: "rahasia", email: "tonni.lius26@gmail.com",type: "admin"}
@@ -95,3 +99,20 @@ function deleteItem(namaItemInput){
         }        
     })
 }
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+    signupBtn.click();
+    return false;
+});
