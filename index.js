e7e96cbff781863f9fc9c6ec44d04309c97228a9
dodol26 <tonni.lius26@gmail.com>
2022-10-13 15:16:13 +0700

    initial database item & database user

---
+++ index.js
@@ -0,0 +1,7 @@
+let dbItem = [
+    {namaItem: "Hack Americano", harga: 10000, img: "#", promo: false}
+]
+
+let dbUser = {
+    dodol: {password: "rahasia", email: "tonni.lius26@gmail.com",type: "admin"}
+}
