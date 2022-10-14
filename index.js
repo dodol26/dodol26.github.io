8853a637d1324acd8f0f0be9f687193ddd1317dc
kibuelll <kibuellyoi34@gmail.com>
2022-10-14 10:54:57 +0700

    push develop

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 let dbItem = [
-  { id: 1, namaItem: "Hack Americano", harga: 10000, img: "./Produk-unggulan/cup-20_1.png", promo: false, desc: 'kopi yang berasa kopi',quantity:0 },
-  { id: 2, namaItem: "item2", harga: 15000, img: "./Produk-unggulan/cup-20_2.png", promo: false, desc: 'kopi yang berasa kopi',quantity:0 },
-  { id: 3, namaItem: "item3", harga: 20000, img: "./Produk-unggulan/slider_part_1.png", promo: false, desc: 'kopi yang berasa kopi',quantity:0 },
-  { id: 4, namaItem: "item4", harga: 25000, img: "./Produk-unggulan/slider_part_2.png", promo: false, desc: 'kopi yang berasa kopi',quantity:0 },
-  { id: 5, namaItem: "item5 ", harga: 25000, img: "./Produk-unggulan/slider_part_2.png", promo: false, desc: 'kopi yang berasa kopi',quantity:0 },
+  { id: 1, namaItem: "Hack Americano", harga: 10000, img: "./Produk-unggulan/cup-20_1.png", promo: false, desc: 'kopi yang berasa kopi', quantity: 0 },
+  { id: 2, namaItem: "item2", harga: 15000, img: "./Produk-unggulan/cup-20_2.png", promo: false, desc: 'kopi yang berasa kopi', quantity: 0 },
+  { id: 3, namaItem: "item3", harga: 20000, img: "./Produk-unggulan/slider_part_1.png", promo: false, desc: 'kopi yang berasa kopi', quantity: 0 },
+  { id: 4, namaItem: "item4", harga: 25000, img: "./Produk-unggulan/slider_part_2.png", promo: false, desc: 'kopi yang berasa kopi', quantity: 0 },
+  { id: 5, namaItem: "item5 ", harga: 25000, img: "./Produk-unggulan/slider_part_2.png", promo: false, desc: 'kopi yang berasa kopi', quantity: 0 },
 ]
 
 dbItem.map((item) => {
@@ -31,7 +31,7 @@ dbItem.map((item) => {
 })
 
 let dbUser = [
-    {email: "tonni.lius26@gmail.com", password: "rahasia", type: "admin"}
+  { email: "tonni.lius26@gmail.com", password: "rahasia", type: "admin" }
 ]
 
 function hide(param) {
@@ -45,40 +45,40 @@ function show(param) {
 let inputPassword = document.getElementById("password-input").value
 let inputEmail = document.getElementById("email-input").value
 
-function signUp(inputEmail, inputPassword, confirmPassword){
-    if (!inputEmail && !inputPassword && !confirmPassword){
-        alert("Invalid input")
-    }else if (!inputEmail && !inputPassword){
-        alert("Mohon masukan email dan password anda")
-    }else if (!inputEmail && !confirmPassword){
-        alert("Mohon masukan email dan konfirmasi password anda")
-    }else if (!inputPassword && !confirmPassword){
-        alert("Mohon masukan password dan konfirmasi password anda")
-    }else if (!inputEmail){
-        alert("Mohon masukan email anda")
-    }else if (!inputPassword){
-        alert("Mohon masukan password anda")
-    }else if (!confirmPassword){
-        alert("Mohon masukan konfirmasi password anda")
-    }else{
-        if (inputPassword !== confirmPassword){
-            alert("Maaf password tidak sama, mohon diperiksa kembali")
-        }else {
-            dbUser.map((item)=>{
-                const {email, password, type} = item
-                if(inputEmail === email){
-                    alert("Maaf email sudah terpakai, mohon gunakan email lain") 
-                }else {
-                    dbUser.push({
-                        email: inputEmail, 
-                        password: inputPassword, 
-                        type: "member"
-                    })
-                    alert("Berhasil")
-                }
-            })
+function signUp(inputEmail, inputPassword, confirmPassword) {
+  if (!inputEmail && !inputPassword && !confirmPassword) {
+    alert("Invalid input")
+  } else if (!inputEmail && !inputPassword) {
+    alert("Mohon masukan email dan password anda")
+  } else if (!inputEmail && !confirmPassword) {
+    alert("Mohon masukan email dan konfirmasi password anda")
+  } else if (!inputPassword && !confirmPassword) {
+    alert("Mohon masukan password dan konfirmasi password anda")
+  } else if (!inputEmail) {
+    alert("Mohon masukan email anda")
+  } else if (!inputPassword) {
+    alert("Mohon masukan password anda")
+  } else if (!confirmPassword) {
+    alert("Mohon masukan konfirmasi password anda")
+  } else {
+    if (inputPassword !== confirmPassword) {
+      alert("Maaf password tidak sama, mohon diperiksa kembali")
+    } else {
+      dbUser.map((item) => {
+        const { email, password, type } = item
+        if (inputEmail === email) {
+          alert("Maaf email sudah terpakai, mohon gunakan email lain")
+        } else {
+          dbUser.push({
+            email: inputEmail,
+            password: inputPassword,
+            type: "member"
+          })
+          alert("Berhasil")
         }
+      })
     }
+  }
 }
 
 let selectedId = document.getElementById("selected-id")
@@ -149,78 +149,79 @@ signupLink.onclick = (() => {
   return false;
 });
 
-function checkLogin(inputEmail, inputPassword){
-    if (!inputEmail && !inputPassword){
-        alert("Invalid input")
-    }else if (!inputEmail){
-        alert("Mohon masukan email anda")
-    }else if (!inputPassword){
-        alert("Mohon masukan password anda")
-    }else{
-        dbUser.map((item)=>{
-            const {email, password, type} = item
-            if (inputEmail === email){
-                if (inputPassword === password){
-                    // if (type === "admin"){
-                    //     show("admin")
-                    // }
-                    alert("Login Berhasil")
-                }else {
-                    alert("Password salah")
-                }
-            }else {
-                alert("Email tidak ditemukan")
-            }
-        })
+function checkLogin(inputEmail, inputPassword) {
+  if (!inputEmail && !inputPassword) {
+    alert("Invalid input")
+  } else if (!inputEmail) {
+    alert("Mohon masukan email anda")
+  } else if (!inputPassword) {
+    alert("Mohon masukan password anda")
+  } else {
+    dbUser.map((item) => {
+      const { email, password, type } = item
+      if (inputEmail === email) {
+        if (inputPassword === password) {
+          // if (type === "admin"){
+          //     show("admin")
+          // }
+          alert("Login Berhasil")
+        } else {
+          alert("Password salah")
+        }
+      } else {
+        alert("Email tidak ditemukan")
+      }
+    })
+  }
+
+  let klik = 0
+  function addToCart(param) {
+    let totalBill = 0
+    let quantity = 0
+    let bill = document.createElement('div')
+    bill.classList.add('list-bill')
+    bill.style.display = 'flex'
+    function deleteFromBills(param) {
+      let quantity = addToCart(param)
+      quantity--
     }
-    
-let klik = 0
-function addToCart(param) {
-  let totalBill = 0
-  let quantity = 0
-  let bill = document.createElement('div')
-  bill.classList.add('list-bill')
-  bill.style.display = 'flex'
-  function deleteFromBills(param) {
-    let quantity = addToCart(param)
-    quantity--
-  }
-  
-  function addToBills(param) {
-    let quantity = addToCart(param)
-    quantity++
-  }
-  bill.style.marginTop = '15px'
-  dbItem.map((item) => {
-    const { id, namaItem, harga, img, promo, desc } = item
-    if (id === param) {
-      bill.innerHTML =
-        `<p style="margin-right:auto;font-weight:700;" class="nama-item">${namaItem}</p>
-    <a class="btn btn-danger" style="font-size:0.75rem;height:25px;" onclick="deleteFromBills(${namaItem})">-</a>
-    <a class="btn btn-success" style="font-size:0.75rem;height:25px;"  onclick="addToBills(${namaItem})">+</a>`
-      document.querySelector('.container-checkout').appendChild(bill)
-      totalBill += harga
+
+    function addToBills(param) {
+      let quantity = addToCart(param)
       quantity++
     }
+    bill.style.marginTop = '15px'
+    dbItem.map((item) => {
+      const { id, namaItem, harga, img, promo, desc } = item
+      if (id === param) {
+        bill.innerHTML =
+          `<p style="margin-right:auto;font-weight:700;" class="nama-item">${namaItem}</p>
+    <a class="btn btn-danger" style="font-size:0.75rem;height:25px;" onclick="deleteFromBills(${namaItem})">-</a>
+    <a class="btn btn-success" style="font-size:0.75rem;height:25px;"  onclick="addToBills(${namaItem})">+</a>`
+        document.querySelector('.container-checkout').appendChild(bill)
+        totalBill += harga
+        quantity++
+      }
 
-    return quantity
-  })
-  let total = document.createElement('div')
-  total.style.display = 'flex'
-  total.innerHTML = `<div style="margin-right:25px;color:blue;fontw-weight:700;">Price</div>
+      return quantity
+    })
+    let total = document.createElement('div')
+    total.style.display = 'flex'
+    total.innerHTML = `<div style="margin-right:25px;color:blue;fontw-weight:700;">Price</div>
 <span class="price" style="color:black;margin-right:150px;"><b>Rp${totalBill}</b></span>
 <span class="price" style="color:black;font-size:1rem;"><b>${quantity}</b></span>`
-  document.querySelector('.container-checkout').appendChild(total)
-  klik++
+    document.querySelector('.container-checkout').appendChild(total)
+    klik++
 
-  if(klik === 5) {
-    let checkout = document.createElement('button')
-    checkout.style.width = "100px"
-    checkout.style.margin = "35px 0"
-    checkout.style.height = "50px"
-    checkout.style.backgroundColor = "pink"
-    checkout.innerText = 'CHECKOUT'
+    if (klik === 5) {
+      let checkout = document.createElement('button')
+      checkout.style.width = "100px"
+      checkout.style.margin = "35px 0"
+      checkout.style.height = "50px"
+      checkout.style.backgroundColor = "pink"
+      checkout.innerText = 'CHECKOUT'
 
-    document.querySelector('.container-checkout').appendChild(checkout)
+      document.querySelector('.container-checkout').appendChild(checkout)
+    }
   }
 }
